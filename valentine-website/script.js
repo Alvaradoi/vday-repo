d6c93a28b4fd55e6991dba6e18273d74a4fb4b1b
Isaac Alvarado <isaacmcalvarado@gmail.com>
2024-02-14 01:55:27 -0800

    Update script.js

--- valentine-website/script.js
+++ valentine-website/script.js
@@ -5,7 +5,15 @@ const initialContent = document.getElementById('initialContent');
 const response = document.getElementById('response');
 
 // Define a list of strings to cycle through for the "Yes" button text
-const noButtonTexts = ["Sure?", "Really sure?", "Think again!", "Are you positive?", "Last chance!", "Yes, I do!"];
+const noButtonTexts = [    
+"Sure?", "Really sure?", "Think again!", "Are you positive?", 
+"Last chance!", "Yes, I do!", "I can do this all day!", 
+"Still no?", "You gotta be kidding me!", "This button is unstoppable!", 
+"Oops, try again!", "How about now?", "Persistence is key!", 
+"Unbreakable spirit!", "I'm getting stronger!", "Aren't you tired yet?", 
+"We can keep going!", "Endurance level 100!", "I have all the time in the world!", 
+"Just click yes already!", "Your persistence is admirable!", "But seriously, yes?"
+];
 let currentTextIndex = 0; // Keep track of which text is currently displayed
 
 // Function to enlarge the "Yes" button and cycle its text
